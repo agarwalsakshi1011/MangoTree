d68801f47519817d858f2841054e93e850abe29f
agarwalsakshi1011 <84676719+agarwalsakshi1011@users.noreply.github.com>
2021-05-29 15:44:34 +0530

    Add files via upload

---
+++ LowerTriangularMatrix.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LowerTriangularMatrix {
+    public static int checkLowerTriangularMatrix(int row,int column,int matrix[][])
+    {
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                if(i<j)
+                {
+                    if(matrix[i][j]!=0)
+                        return 0;
+                }
+            }
+        }
+        return 1;
+    }
+    public  static  void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int column=sc.nextInt();
+        if(row!=column)
+            System.out.println("Not a Square Matrix");
+        else {
+            int[][] matrix = new int[row][column];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            if(checkLowerTriangularMatrix(row,column,matrix)==1)
+
+                System.out.println("yes");
+            else
+                System.out.println("no");
+
+        }
+
+    }
+}
